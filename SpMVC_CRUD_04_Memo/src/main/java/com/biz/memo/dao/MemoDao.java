4d028325952e9b3f0c378618cb2c171faaac34df
whdtjr3973 <whdtjr3973@naver.com>
2019-07-19 16:30:10 +0900

    2019-07-19

---
+++ SpMVC_CRUD_04_Memo/src/main/java/com/biz/memo/dao/MemoDao.java
@@ -0,0 +1,14 @@
+package com.biz.memo.dao;
+
+import java.util.List;
+
+import com.biz.memo.model.MemoVO;
+
+public interface MemoDao {
+
+	public List<MemoVO> selectAll();
+	public int insert(MemoVO memoVO);
+	public MemoVO findBySeq(long mo_seq);
+	public int update(MemoVO memoVO);
+	
+}
